5bd8f5062418bc3b9d56ce3f8ac150c565a16f53
Jimoochen <52712083+Jimoochen@users.noreply.github.com>
2021-02-02 10:40:22 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+1+1
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+from  IPython.display  import  Latex
+
+
+# In[3]:
+
+
+Latex ( r"$f(x) = 3x + 7$" )
+
+
+# In[4]:
+
+
+Latex ( r"$\sum_{i=1 }^n a_i=0$" )
+
+
+# In[ ]:
+
+
+
+
